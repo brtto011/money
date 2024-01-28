5940ab3c710f22c9a6d3fcfa8c920926dbfb11cf
Davi <davimachadocruz@gmail.com>
2024-01-28 18:30:55 -0300

    Animação adicionada na aside

--- wtw-yocy-tcu/components/aside.php
+++ wtw-yocy-tcu/components/aside.php
@@ -1,118 +1,136 @@
-<aside class="left-sidebar" style="background-color: #fff; box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 0px 16px -8px;
-">
-      <!-- Sidebar scroll-->
-      <div class="scroll-sidebar">
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav">
-          <ul id="sidebarnav" class="pt-4">
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="https://www.subwaycash.cloud/wtw-yocy-tcu/" aria-expanded="false"  style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;"
-                  class="mdi mdi-view-dashboard"></i><span class="hide-menu">Página inicial</span></a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../usuarios" aria-expanded="false"  style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;"
-                  class="mdi mdi-account"></i><span class="hide-menu">Usuários</span></a>
-            </li>
-
-
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../planos" aria-expanded="false"  style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;"
-                  class="mdi mdi-square-inc-cash"></i><span class="hide-menu">Afiliados</span></a>
-            </li>
-
-
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../webhook" aria-expanded="false"  style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;"
-                  class="mdi mdi-webhook"></i><span class="hide-menu">WebHook </span></a>
-            </li>
-
-
-
-            <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../notificacao" aria-expanded="false"  style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;"
-                  class="mdi mdi-near-me"></i><span class="hide-menu">Notificações </span></a>
-            </li>
-
-
 <style>
-    .sidebar-nav .has-arrow:after {
-    position: absolute;
-    content: "";
-    width: 7px;
-    height: 7px;
-    border-width: 1px 0 0 1px;
-    border-style: solid;
-    border-color: #343a40;
-    margin-left: 10px;
-    transform: rotate(135deg) translate(0, -50%);
-    transform-origin: top;
-    top: 26px;
-    right: 15px;
-    transition: all 0.3s ease-out;
-    }
+  * {
+    transition: all 0.3s ease;
+  }
 </style>
 
+<aside class="left-sidebar" style="background-color: #fff; box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 0px 16px -8px;
+">
+  <!-- Sidebar scroll-->
+  <div class="scroll-sidebar">
+    <!-- Sidebar navigation-->
+    <nav class="sidebar-nav">
+      <ul id="sidebarnav" class="pt-4">
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="https://www.subwaycash.cloud/wtw-yocy-tcu/"
+            aria-expanded="false" style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-view-dashboard"></i><span class="hide-menu">Página inicial</span></a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../usuarios" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-account"></i><span class="hide-menu">Usuários</span></a>
+        </li>
 
-            <li class="sidebar-item">
-              <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false" style="color: #343a40; font-weight: 700; font-size: 18px;">
-                 <i  style="color: #343a40;" class="mdi mdi-square-inc-cash"></i>
-                <span class="hide-menu">Financeiro </span>
-              </a>
-              <ul aria-expanded="false" class="collapse first-level" style="color: #343a40">
-                <li class="sidebar-item" style="color: #343a40">
-                  <a href="../depositos" class="sidebar-link">
-                    <i class="mdi mdi-cash" style="color: #343a40">
 
-                    </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Depositos </span></a>
-                </li>
-                <li class="sidebar-item">
-                  <a href="../saques" class="sidebar-link">
-                    <i class="mdi mdi-cash" style="color: #343a40"></i>
-                    <span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Saques </span></a>
-                </li>
-                <li class="sidebar-item ">
-                  <a href="../saques-afiliados" class="sidebar-link">
-                    <i class="mdi mdi-cash" style="color: #343a40">
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../planos" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-square-inc-cash"></i><span class="hide-menu">Afiliados</span></a>
+        </li>
 
-                    </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Saques Afiliados </span></a>
-                </li>
-              </ul>
+
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../webhook" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-webhook"></i><span class="hide-menu">WebHook </span></a>
+        </li>
+
+
+
+        <li class="sidebar-item">
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../notificacao" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-near-me"></i><span class="hide-menu">Notificações </span></a>
+        </li>
+
+
+        <style>
+          .sidebar-nav .has-arrow:after {
+            position: absolute;
+            content: "";
+            width: 7px;
+            height: 7px;
+            border-width: 1px 0 0 1px;
+            border-style: solid;
+            border-color: #343a40;
+            margin-left: 10px;
+            transform: rotate(135deg) translate(0, -50%);
+            transform-origin: top;
+            top: 26px;
+            right: 15px;
+            transition: all 0.3s ease-out;
+          }
+        </style>
+
+
+        <li class="sidebar-item">
+          <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;">
+            <i style="color: #343a40;" class="mdi mdi-square-inc-cash"></i>
+            <span class="hide-menu">Financeiro </span>
+          </a>
+          <ul aria-expanded="false" class="collapse first-level" style="color: #343a40">
+            <li class="sidebar-item" style="color: #343a40">
+              <a href="../depositos" class="sidebar-link">
+                <i class="mdi mdi-cash" style="color: #343a40">
+
+                </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Depositos
+                </span></a>
             </li>
-
-
             <li class="sidebar-item">
-              <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false" style="color: #343a40; font-weight: 700; font-size: 18px;"><i  style="color: #343a40;" class="mdi mdi-settings"></i>
-                <span class="hide-menu">Configurações </span>
-              </a>
-              <ul aria-expanded="false" class="collapse first-level">
-                <li class="sidebar-item ">
-                  <a href="../gateway" class="sidebar-link">
-                    <i class="mdi mdi-cash" style="color: #343a40">
-
-                    </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Gateway </span></a>
-                </li>
-                <li class="sidebar-item">
-                  <a href="../pixels" class="sidebar-link">
-                    <i class="mdi mdi-code-tags" style="color: #343a40"></i>
-                    <span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Pixels </span></a>
-                </li>
-                <li class="sidebar-item ">
-                  <a href="../config" class="sidebar-link">
-                    <i class="mdi mdi-account" style="color: #343a40">
-
-                    </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Nome do Game </span></a>
-                </li>
-              </ul>
+              <a href="../saques" class="sidebar-link">
+                <i class="mdi mdi-cash" style="color: #343a40"></i>
+                <span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Saques </span></a>
             </li>
+            <li class="sidebar-item ">
+              <a href="../saques-afiliados" class="sidebar-link">
+                <i class="mdi mdi-cash" style="color: #343a40">
 
-
-
-
-
-            
+                </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Saques Afiliados
+                </span></a>
+            </li>
           </ul>
-        </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
+        </li>
+
+
+        <li class="sidebar-item">
+          <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"
+            style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
+              class="mdi mdi-settings"></i>
+            <span class="hide-menu">Configurações </span>
+          </a>
+          <ul aria-expanded="false" class="collapse first-level">
+            <li class="sidebar-item ">
+              <a href="../gateway" class="sidebar-link">
+                <i class="mdi mdi-cash" style="color: #343a40">
+
+                </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Gateway
+                </span></a>
+            </li>
+            <li class="sidebar-item">
+              <a href="../pixels" class="sidebar-link">
+                <i class="mdi mdi-code-tags" style="color: #343a40"></i>
+                <span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Pixels </span></a>
+            </li>
+            <li class="sidebar-item ">
+              <a href="../config" class="sidebar-link">
+                <i class="mdi mdi-account" style="color: #343a40">
+
+                </i><span class="hide-menu" style="color: #343a40; font-weight: 700; font-size: 15px;"> Nome do Game
+                </span></a>
+            </li>
+          </ul>
+        </li>
+
+
+
+
+
+
+      </ul>
+    </nav>
+    <!-- End Sidebar navigation -->
+  </div>
+  <!-- End Sidebar scroll-->
+</aside>
