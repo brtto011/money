4a86b7a0c6e369358d36d8e323720494d125d28f
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-01-28 20:17:36 -0300

    att adm

--- wtw-yocy-tcu/components/aside.php
+++ wtw-yocy-tcu/components/aside.php
@@ -12,7 +12,7 @@
     <nav class="sidebar-nav">
       <ul id="sidebarnav" class="pt-4">
         <li class="sidebar-item">
-          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="https://www.subwaycash.cloud/wtw-yocy-tcu/"
+          <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../"
             aria-expanded="false" style="color: #343a40; font-weight: 700; font-size: 18px;"><i style="color: #343a40;"
               class="mdi mdi-view-dashboard"></i><span class="hide-menu">Página inicial</span></a>
         </li>
